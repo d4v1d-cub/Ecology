0f38ea8dfc4563440b9465623305eb5985e092a7
d4v1d-cub <davidmachadop@gmail.com>
2025-10-23 12:14:08 +0200

    processing data

--- processing_data/IBMF/summary_statistics_IBMF_T0_seq_ER.py
+++ processing_data/IBMF/summary_statistics_IBMF_T0_seq_ER.py
@@ -26,7 +26,8 @@ def summary_statistics(path, filename):
     all_lines = fin.readlines()
     fin.close()
     if len(all_lines) > 0:
-        av_time = 0.0
+        av_time_conv = 0.0
+        av_time_conv_sqr = 0.0
         av_num_div = 0.0
         samples_div_m = 0
         av_m = 0.0
@@ -37,32 +38,37 @@ def summary_statistics(path, filename):
             line_split = line.split()
             if len(line_split) < 11:  # Skip lines that do not have enough data
                 continue
-            av_time += int(line_split[0])
             av_num_div += int(line_split[4])
             if line_split[1] != "1":
                 samples_div_m += 1
             else:
+                av_time_conv += int(line_split[0])
+                av_time_conv_sqr += int(line_split[0]) * int(line_split[0])
                 runtime_conv += float(line_split[10])
                 runtime_conv_sqr += float(line_split[10]) * float(line_split[10])
             av_m += float(line_split[2])
             av_m_sqr += float(line_split[2]) * float(line_split[2])
-        av_time /= len(all_lines)
         av_num_div /= len(all_lines)
         av_m /= len(all_lines)
         av_m_sqr /= len(all_lines)
         std_av_m = np.sqrt(abs(av_m_sqr - av_m * av_m))
         if (len(all_lines) - samples_div_m) > 0:
+            av_time_conv /= (len(all_lines) - samples_div_m)
+            av_time_conv_sqr /= (len(all_lines) - samples_div_m)
+            std_time_conv = np.sqrt(abs(av_time_conv_sqr - av_time_conv * av_time_conv))
             runtime_conv /= (len(all_lines) - samples_div_m)
             runtime_conv_sqr /= (len(all_lines) - samples_div_m)
             std_runtime_conv = np.sqrt(abs(runtime_conv_sqr - runtime_conv * runtime_conv))
         else:
+            av_time_conv = "nodata"
+            std_time_conv = "nodata"
             runtime_conv = "nodata"
             std_runtime_conv = "nodata"
-        return av_time, av_num_div, samples_div_m, av_m, std_av_m, \
+        return av_time_conv, std_time_conv, av_num_div, samples_div_m, av_m, std_av_m, \
                runtime_conv, std_runtime_conv, len(all_lines), True
     else:
         print(f"No data found in file {filename}. Returning zeros.")
-        return 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.0, 0, False
+        return 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.0, 0, False
 
 
 def get_all_vals(path, T, lda, eps, avn0, dn, ninitconds, tol, max_iter, damping, nseq):
@@ -70,9 +76,9 @@ def get_all_vals(path, T, lda, eps, avn0, dn, ninitconds, tol, max_iter, damping
     sorted_data = filter_files(path, T, lda, eps, avn0, dn, ninitconds, tol, max_iter, damping, nseq)
     vals_list = []
     for filename, N, c, mu, sigma in sorted_data:
-        av_time, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples, found = summary_statistics(path, filename)
+        av_time_conv, std_time_conv, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples, found = summary_statistics(path, filename)
         if found:
-            vals_list.append((N, c, mu, sigma, av_time, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples))
+            vals_list.append((N, c, mu, sigma, av_time_conv, std_time_conv, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples))
         print(f'Processed N={N}  c={c}  mu={mu}   sigma={sigma}')
     return vals_list
 
@@ -84,13 +90,15 @@ def print_summary(path_in, path_out, T, lda, eps, avn0, dn, ninitconds, tol, max
     fout.write("# N  c  mu  sigma  av_time  av_div  samples_div  prob_div  error_prob  av_m  error_av_m  runtime(s)  std_runtime(s) nsamples\n")
     vals_list = get_all_vals(path_in, T, lda, eps, avn0, dn, ninitconds, tol, max_iter, damping, nseq)
     for vals in vals_list:
-        N, c, mu, sigma, av_time, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples = vals
+        N, c, mu, sigma, av_time_conv, std_time_conv, av_num_div, samples_div_m, av_m, std_av_m, runtime_conv, std_runtime_conv, nsamples = vals
         prob = samples_div_m / nsamples if nsamples > 0 else 0.0
         error = np.sqrt(prob * (1 - prob) / nsamples) if nsamples > 0 else 0.0
         if runtime_conv != "nodata":
+            av_time_conv = f'{av_time_conv:.{6}f}'
+            std_time_conv = f'{std_time_conv:.{6}f}'
             runtime_conv = f'{runtime_conv:.{6}f}'
             std_runtime_conv = f'{std_runtime_conv:.{6}f}'
-        fout.write(f'{N} {int(c * 10 ** ndigits)} {int(mu * 10 ** ndigits)} {int(sigma * 10 ** ndigits)} {av_time:.{6}f} {av_num_div:.{6}f} {samples_div_m} {samples_div_m / nsamples:.{6}f} {error:.{6}f} {av_m:.{6}f} {std_av_m:.{6}f} {runtime_conv} {std_runtime_conv} {nsamples}\n')
+        fout.write(f'{N} {int(c * 10 ** ndigits)} {int(mu * 10 ** ndigits)} {int(sigma * 10 ** ndigits)} {av_time_conv} {std_time_conv} {av_num_div:.{6}f} {samples_div_m} {samples_div_m / nsamples:.{6}f} {error:.{6}f} {av_m:.{6}f} {std_av_m:.{6}f} {runtime_conv} {std_runtime_conv} {nsamples}\n')
     fout.close()
 
 
--- processing_data/Langevin/summary_statistics_Langevin_T0_directed_ER.py
+++ processing_data/Langevin/summary_statistics_Langevin_T0_directed_ER.py
@@ -47,7 +47,8 @@ def summary_statistics(path, filename):
     all_lines = fin.readlines()
     fin.close()
     if len(all_lines) > 0:
-        av_time = 0.0
+        av_time_conv = 0.0
+        av_time_conv_sqr = 0.0
         samples_div = 0
         samples_multiple_eq = 0
         runtime_conv = 0.0
@@ -56,27 +57,32 @@ def summary_statistics(path, filename):
         for line in all_lines:
             if not line.startswith('#'):
                 line_split = line.split()
-                av_time += float(line_split[3])
                 if line_split[5] == "0":
                     samples_div += 1
                 else:
+                    av_time_conv += float(line_split[3])
+                    av_time_conv_sqr += float(line_split[3]) * float(line_split[3])
                     runtime_conv += float(line_split[11]) / 1000
                     runtime_conv_sqr += (float(line_split[11]) / 1000) * (float(line_split[11]) / 1000)
                 if line_split[9] == "1" or line_split[7] == "1":
                     samples_multiple_eq += 1
                 nsamples += 1
-        av_time /= nsamples
         if (nsamples - samples_div) > 0:
+            av_time_conv /= (nsamples - samples_div)
+            av_time_conv_sqr /= (nsamples - samples_div)
+            std_time_conv = np.sqrt(abs(av_time_conv_sqr - av_time_conv * av_time_conv))
             runtime_conv /= (nsamples - samples_div)
             runtime_conv_sqr /= (nsamples - samples_div)
             std_runtime_conv = np.sqrt(abs(runtime_conv_sqr - runtime_conv * runtime_conv))
         else:
+            av_time_conv = "nodata"
+            std_time_conv = "nodata"
             runtime_conv = "nodata"
             std_runtime_conv = "nodata"
-        return av_time, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples, True
+        return av_time_conv, std_time_conv, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples, True
     else:
         print(f"No data found in file {filename}. Returning zeros.")
-        return 0.0, 0, 0, 0.0, 0.0, 0, False
+        return 0.0, 0.0, 0, 0, 0.0, 0.0, 0, False
 
 
 def get_all_vals(path, eps, lda, tol_fixed_point, T):
@@ -84,9 +90,9 @@ def get_all_vals(path, eps, lda, tol_fixed_point, T):
     sorted_data = filter_files(path, eps, lda, tol_fixed_point, T)
     vals_list = []
     for filename, N, c, mu, sigma in sorted_data:
-        av_time, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples, found = summary_statistics(path, filename)
+        av_time_conv, std_time_conv, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples, found = summary_statistics(path, filename)
         if found:
-            vals_list.append((N, c, mu, sigma, av_time, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples))
+            vals_list.append((N, c, mu, sigma, av_time_conv, std_time_conv, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples))
         print(f'Processed N={N}  c={c}   mu={mu}   sigma={sigma}')
     return vals_list
 
@@ -95,16 +101,18 @@ def get_all_vals(path, eps, lda, tol_fixed_point, T):
 
 def print_summary(path_in, path_out, eps, lda, tol_fixed_point, T, ndigits):
     fout = open(f'{path_out}/Lotka-Volterra_summary_epsilon_{eps}_Directed_Trivial_lambda_{lda}_tol_{tol_fixed_point}_T_{T}.txt', 'w')
-    fout.write("#N  c  mu  sigma  av_time  samples_div  samples_mult_eq  prob_div  error_prob  runtime(s)  std_runtime(s)  nsamples\n")
+    fout.write("#N  c  mu  sigma  av_time_conv  std_time_conv  samples_div  samples_mult_eq  prob_div  error_prob  runtime(s)  std_runtime(s)  nsamples\n")
     vals_list = get_all_vals(path_in, eps, lda, tol_fixed_point, T)
     for vals in vals_list:
-        N, c, mu, sigma, av_time, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples = vals
+        N, c, mu, sigma, av_time_conv, std_time_conv, samples_div, samples_multiple_eq, runtime_conv, std_runtime_conv, nsamples = vals
         prob = samples_div / nsamples if nsamples > 0 else 0.0
         error = np.sqrt(prob * (1 - prob) / nsamples) if nsamples > 0 else 0.0
         if runtime_conv != "nodata":
+            av_time_conv = f'{av_time_conv:.{6}f}'
+            std_time_conv = f'{std_time_conv:.{6}f}'
             runtime_conv = f'{runtime_conv:.{6}f}'
             std_runtime_conv = f'{std_runtime_conv:.{6}f}'
-        fout.write(f"{N} {int(c * 10 ** ndigits)} {int(mu * 10 ** ndigits)} {int(sigma * 10 ** ndigits)} {av_time:.6f} {samples_div} {samples_multiple_eq} {prob:.6f} {error:.6f} {runtime_conv} {std_runtime_conv} {nsamples}\n")
+        fout.write(f"{N} {int(c * 10 ** ndigits)} {int(mu * 10 ** ndigits)} {int(sigma * 10 ** ndigits)} {av_time_conv} {std_time_conv} {samples_div} {samples_multiple_eq} {prob:.6f} {error:.6f} {runtime_conv} {std_runtime_conv} {nsamples}\n")
     fout.close()
 
 
