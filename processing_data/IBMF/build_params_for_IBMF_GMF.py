b3ad3b8f5093a63812545ca56411eb97b890ae7a
d4v1d-cub <davidmachadop@gmail.com>
2026-01-20 16:03:28 +0100

    building params IBMF deaths

--- processing_data/IBMF/build_params_for_IBMF_GMF.py
+++ processing_data/IBMF/build_params_for_IBMF_GMF.py
@@ -67,6 +67,52 @@ def print_params(path_in, filein, path_out, fileout, dpar_trans, pos_par_fixed,
     return already_printed, counter
 
 
+
+def print_params_search_below_line(path_in, filein, path_out, fileout, dpar_trans, pos_par_fixed, 
+                 pos_par_trans_1, pos_par_trans_2, shift_above, par_fixed_list, eps, seed_block, nsampl_each, 
+                 already_printed=None):
+    transitions = read_transitions(f'{path_in}/{filein}', pos_par_fixed, 
+                                         pos_par_trans_1, pos_par_trans_2)
+    counter = 0
+    par_in_trans = transitions.keys()
+    if already_printed is None:
+        already_printed = []
+    with open(f'{path_out}/{fileout}', 'a') as fo:
+        for par_fixed in par_fixed_list:
+            if par_fixed in par_in_trans:
+                _, trans_max = transitions[par_fixed]
+                min_val = 0
+                max_val = trans_max + shift_above
+                par_list = np.arange(min_val, max_val + dpar_trans / 2, dpar_trans)
+                for par in par_list:
+                    if (par_fixed, par) not in already_printed:
+                        fo.write(f"{eps} {par_fixed:.3f} {par:.3f} {seed_block} {nsampl_each}\n")
+                        already_printed.append((par_fixed, par))
+                        counter += 1
+            else:
+                closest_above = max(par_in_trans)
+                closest_below = min(par_in_trans)
+                for par in par_in_trans:
+                    if par < par_fixed and par > closest_below:
+                        closest_below = par
+                    if par > par_fixed and par < closest_above:
+                        closest_above = par
+                _, trans_above_max = transitions[closest_above]
+                _, trans_below_max = transitions[closest_below]
+                min_val = 0
+                max_val = max(trans_above_max, trans_below_max) + shift_above
+                par_list = np.arange(min_val, max_val + dpar_trans / 2, dpar_trans)
+                for par in par_list:
+                    if (par_fixed, par) not in already_printed:
+                        fo.write(f"{eps} {par_fixed:.3f} {par:.3f} {seed_block} {nsampl_each}\n")
+                        already_printed.append((par_fixed, par))
+                        counter += 1
+        
+    print(f"Parameters saved to {path_out}/{fileout}")
+    print(f"Total parameters: {counter}")
+    return already_printed, counter
+
+
 def print_params_missing(path_in_trans, path_in_already, filein_trans, filein_already, path_out, fileout, 
                          dpar_trans, pos_par_fixed, pos_par_trans_1, pos_par_trans_2, shift_below, shift_above, 
                          par_fixed_list, eps, seed_block, nsampl_each):
@@ -169,6 +215,59 @@ def main():
     #                 par_fixed_list, eps, seed_block, nsampl_each)
         
 
+    # IBMF
+
+    dpar_trans = 0.01
+    ndigits = 3
+    shift_above = 0.02
+
+    eps = "0.000"
+    seed_block = "1"
+    nsampl_each = "10000"
+
+    # path_in = "/media/david/Data/UH/Grupo_de_investigacion/Ecology/Results/IBMF/"
+    path_in = "/media/david/Seagate Expansion Drive/Salva/Salva_Data_Investigacion/Grupo_de_investigacion/Ecology/Results/IBMF"
+    # path_in = "/media/david/Seagate Expansion Drive/Salva/Salva_Data_Investigacion/Grupo_de_investigacion/Ecology/Langevin/Results"
+    N_list = [1024, 2048, 4096]
+    dpar_fixed = 0.03
+    par_fixed_start = -0.330
+    par_fixed_end = 0.36
+    par_fixed_list = np.arange(par_fixed_start, par_fixed_end + dpar_fixed / 2, dpar_fixed)
+    for i in range(len(par_fixed_list)):
+        par_fixed_list[i] = round(par_fixed_list[i], ndigits)
+
+    path_out = "/media/david/Seagate Expansion Drive/Salva/Salva_Data_Investigacion/Grupo_de_investigacion/Ecology/Scripts/Dresden/IBMF"
+
+    for N in N_list:
+        filein = f'IBMF_seq_RRG_T_0.000_lambda_0.000_PD_Lotka_Volterra_transitions_mult_av0_0.5_dn_0.5_ninitconds_10_tol_1e-6_maxiter_10000_eps_0.000_N_{N}_c_3_damping_0.2_nseq_1.txt'
+        
+        fileout = f'params_IBMF_T0_seq_dif_init_conds_eps0_N_{N}.txt'
+        pos_par_fixed = 0
+        pos_par_trans_1 = 1
+        pos_par_trans_2 = 2
+
+        print_params_search_below_line(path_in, filein, path_out, fileout, dpar_trans, pos_par_fixed, 
+                                       pos_par_trans_1, pos_par_trans_2, shift_above, 
+                                       par_fixed_list, eps, seed_block, nsampl_each)
+
+
+    N_list = [8192, 16384, 32768]
+
+    filein = f'IBMF_seq_RRG_T_0.000_lambda_0.000_PD_Lotka_Volterra_transitions_mult_av0_0.5_dn_0.5_ninitconds_10_tol_1e-6_maxiter_10000_eps_0.000_N_4096_c_3_damping_0.2_nseq_1.txt'
+        
+
+    for N in N_list:
+        
+        fileout = f'params_IBMF_T0_seq_dif_init_conds_eps0_N_{N}.txt'
+        pos_par_fixed = 0
+        pos_par_trans_1 = 1
+        pos_par_trans_2 = 2
+
+        print_params_search_below_line(path_in, filein, path_out, fileout, dpar_trans, pos_par_fixed, 
+                                       pos_par_trans_1, pos_par_trans_2, shift_above, 
+                                       par_fixed_list, eps, seed_block, nsampl_each)
+    
+
     # EPSILON = "0.0" (ASYMMETRIC)  params: (mu, sigma)
 
     # IBMF
@@ -237,42 +336,42 @@ def main():
 
     # EPSILON = "0.5" (ASYMMETRIC)  params: (mu, sigma)
 
-    dpar_trans = 0.004
-    shift_below = 0.004
-    shift_above = 0.004
-    ndigits = 3
+    # dpar_trans = 0.004
+    # shift_below = 0.004
+    # shift_above = 0.004
+    # ndigits = 3
 
-    eps = "0.500"
-    seed_block = "1"
-    nsampl_each = "10000"
+    # eps = "0.500"
+    # seed_block = "1"
+    # nsampl_each = "10000"
 
-    # IBMF
-    path_in = "/mnt/d/Research/Ecology/Langevin/Results"
-    N_list = [1024, 4096]
-    dpar_fixed = 0.012
-    par_fixed_start = -0.333
-    par_fixed_end = 0.351
-    par_fixed_list = np.arange(par_fixed_start, par_fixed_end + dpar_fixed / 2, dpar_fixed)
-    for i in range(len(par_fixed_list)):
-        par_fixed_list[i] = round(par_fixed_list[i], ndigits)
+    # # IBMF
+    # path_in = "/mnt/d/Research/Ecology/Langevin/Results"
+    # N_list = [1024, 4096]
+    # dpar_fixed = 0.012
+    # par_fixed_start = -0.333
+    # par_fixed_end = 0.351
+    # par_fixed_list = np.arange(par_fixed_start, par_fixed_end + dpar_fixed / 2, dpar_fixed)
+    # for i in range(len(par_fixed_list)):
+    #     par_fixed_list[i] = round(par_fixed_list[i], ndigits)
 
-    for N in N_list:
-        filein_mult = f'Lotka-Volterra_transition_mult_epsilon_0.5_Partially_AsymGauss_lambda_1e-06_tol_1e-08_N_{N}_c_3.00_T_0.0.txt'
-        filein_div = f'Lotka-Volterra_transition_div_epsilon_0.5_Partially_AsymGauss_lambda_1e-06_tol_1e-08_N_{N}_c_3.00_T_0.0.txt'
+    # for N in N_list:
+    #     filein_mult = f'Lotka-Volterra_transition_mult_epsilon_0.5_Partially_AsymGauss_lambda_1e-06_tol_1e-08_N_{N}_c_3.00_T_0.0.txt'
+    #     filein_div = f'Lotka-Volterra_transition_div_epsilon_0.5_Partially_AsymGauss_lambda_1e-06_tol_1e-08_N_{N}_c_3.00_T_0.0.txt'
 
-        path_out = "/mnt/d/Research/Ecology/Scripts/Dresden/IBMF"
-        fileout = f'params_IBMF_T0_seq_dif_init_conds_eps05_N_{N}.txt'
-        pos_par_fixed = 0
-        pos_par_trans_1 = 1
-        pos_par_trans_2 = 2
+    #     path_out = "/mnt/d/Research/Ecology/Scripts/Dresden/IBMF"
+    #     fileout = f'params_IBMF_T0_seq_dif_init_conds_eps05_N_{N}.txt'
+    #     pos_par_fixed = 0
+    #     pos_par_trans_1 = 1
+    #     pos_par_trans_2 = 2
 
-        already_printed, counter1 = print_params(path_in, filein_mult, path_out, fileout, dpar_trans, pos_par_fixed, 
-                                                 pos_par_trans_1, pos_par_trans_2, shift_below, shift_above, 
-                                                 par_fixed_list, eps, seed_block, nsampl_each)
-        _, counter2 = print_params(path_in, filein_div, path_out, fileout, dpar_trans, pos_par_fixed, 
-                                  pos_par_trans_1, pos_par_trans_2, shift_below, shift_above, 
-                                  par_fixed_list, eps, seed_block, nsampl_each, already_printed)
-        print(f"Total unique parameters for N={N}: {counter1 + counter2}")
+    #     already_printed, counter1 = print_params(path_in, filein_mult, path_out, fileout, dpar_trans, pos_par_fixed, 
+    #                                              pos_par_trans_1, pos_par_trans_2, shift_below, shift_above, 
+    #                                              par_fixed_list, eps, seed_block, nsampl_each)
+    #     _, counter2 = print_params(path_in, filein_div, path_out, fileout, dpar_trans, pos_par_fixed, 
+    #                               pos_par_trans_1, pos_par_trans_2, shift_below, shift_above, 
+    #                               par_fixed_list, eps, seed_block, nsampl_each, already_printed)
+    #     print(f"Total unique parameters for N={N}: {counter1 + counter2}")
 
     return 0
 
