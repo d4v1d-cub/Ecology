a128a86baef4c5103425d8e41cbca6d78921c51d
d4v1d-cub <davidmachadop@gmail.com>
2026-08-21 21:44:55 -0400

    fixing the printing

--- processing_data/IBMF/gather_nfixedpoints_IBMF.py
+++ processing_data/IBMF/gather_nfixedpoints_IBMF.py
@@ -30,11 +30,12 @@ def parse_summary_file(path, ninitcond, num_seq):
         if not line or line.startswith('#'):
             continue
         nfp += 1
-        if len(line) < 2:
+        line_split = line.split()
+        if len(line_split) < 2:
             print(f"The fix point No. {nfp} in file {path} does not have a second column")
             print(f"Line is: {line}")
             continue
-        ratio_sum += float(line.split()[1])
+        ratio_sum += float(line_split[1])
     attempts = num_seq * ninitcond
     n_converged = int(round(ratio_sum * attempts))
     return nfp, n_converged
